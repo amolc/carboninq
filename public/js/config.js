a2e11f7a94623d2799ee6958ad3fbddd17493025
appasaheb3 <user-pc@user-pc-PC>
2018-05-29 15:52:08 +0530

    commit

--- public/js/config.js
+++ public/js/config.js
@@ -2,9 +2,9 @@
 
 //if(document.location.hostname == "priya.com" || document.location.hostname == "www.priya.com"){
 	var business_id = { business_id: 78 };
-//	var baseurl = "https://www.sparksinasia.com/api/" ;
-//    var imageURL = "https://www.sparksinasia.com/assets/"; 
+	var baseurl = "https://www.sparksinasia.com/api/" ;
+    var imageURL = "https://www.sparksinasia.com/assets/"; 
       
-    var baseurl = "http://localhost:2000/api/" ;
-    var imageURL = "http://localhost:2000/assets/";
+//    var baseurl = "http://localhost:2000/api/" ;
+//    var imageURL = "http://localhost:2000/assets/";
 
