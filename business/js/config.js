607dfe9f9bb48b0f724fb0db8d0b4be8cbf40d6f
amolc <amol.chawathe@fountaintechies.com>
2018-09-12 09:26:52 +0800

    Config file for business updated

--- business/js/config.js
+++ business/js/config.js
@@ -1,8 +1,8 @@
 
 
-	// var baseURL = "https://api.fountaintechies.com/api/";
-	// var imageURL = "https://api.fountaintechies.com/assets/web/78/";
+	var baseURL = "https://api.fountaintechies.com/api/";
+	var imageURL = "https://api.fountaintechies.com/assets/web/78/";
 
-	var baseURL = "http://localhost:2000/api/";
-	var imageURL = "http://localhost:2000/assets/web/78";
+	// var baseURL = "http://localhost:2000/api/";
+	// var imageURL = "http://localhost:2000/assets/web/78";
 //
