dbc86b5f72688d9ac0381528b0818361b033378b
amolc <amol.chawathe@fountaintechies.com>
2018-09-16 23:33:56 +0800

    Payment gateway goes live

--- public/js/paymentMethod.js
+++ public/js/paymentMethod.js
@@ -20,8 +20,8 @@ $(window).scroll(function (e) {
     }
 });
 
-// Stripe.setPublishableKey('pk_live_Myl1OMbipQMGAMDnqf6SZ3Gv'); //carboninq account
-Stripe.setPublishableKey('pk_test_OKKZyHD6nnZujaeDy0ks4fWa'); //carboninq account
+Stripe.setPublishableKey('pk_live_Myl1OMbipQMGAMDnqf6SZ3Gv'); //carboninq account
+//Stripe.setPublishableKey('pk_test_OKKZyHD6nnZujaeDy0ks4fWa'); //carboninq account
 
 function cartCount() {
     if (localStorage.getItem('cart_data') != null) {
